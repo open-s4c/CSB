79def5a1155f5ef7fdf7ccf6410aec5e4a191fa5
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-05-22 12:13:34 +0300

    redirect
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/bm_process.py
+++ bm-runner/bm_process.py
@@ -40,15 +40,16 @@ class Process(ExecutionUnit):
         commands = (
             f"{self.CMD_WHILE_NOT_START}{change_dir}taskset --cpu-list {self.core_set} {command}"
         )
-        with open(resolve_path(self.output_file), "w") as outfile:
-            self.process = subprocess.Popen(
-                commands,
-                shell=True,
-                stdout=outfile,
-                stderr=subprocess.DEVNULL,
-                preexec_fn=preexec_process,
-                cwd=self.home_dir,
-            )
+        with open(resolve_path(self.err_file), "w") as err_file:
+            with open(resolve_path(self.output_file), "w") as outfile:
+                self.process = subprocess.Popen(
+                    commands,
+                    shell=True,
+                    stdout=outfile,
+                    stderr=err_file,
+                    preexec_fn=preexec_process,
+                    cwd=self.home_dir,
+                )
         bm_log(f"launched process {self.name} with {commands}")
         return True
 
