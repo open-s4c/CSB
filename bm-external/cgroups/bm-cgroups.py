6e03d62ce040ef6139c9a681d56358afec8aea67
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-06-03 16:20:41 +0300

    Update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-external/cgroups/bm-cgroups.py
+++ bm-external/cgroups/bm-cgroups.py
@@ -19,11 +19,13 @@ def launch_container(index):
     start_time = time.perf_counter()
     try:
         create_out = shell_out(command = f"sudo runc run -d {container_name}", print_shell_cmd=False,
-                            print_input = False,
+                            print_output = False,
+                            print_input  = False,
                             output_is_log=False,
                             print_file_shell_cmd=False,)
         delete_out = shell_out(command = f"sudo runc delete -f {container_name}",
                             print_input = False,
+                            print_output = False,
                             print_shell_cmd=False,
                             output_is_log=False,
                             print_file_shell_cmd=False,)
