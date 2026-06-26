f82edc2af22baa767688839edfca672d57df796a
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-06-26 18:08:56 +0300

    append to output
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/monitors/bpftrace.py
+++ bm-runner/monitors/bpftrace.py
@@ -4,23 +4,24 @@ from monitors.monitor import Monitor
 from utils.process import BackgroundProcess
 from bm_utils import resolve_path
 import os
-from pathlib import Path
 from utils.logger import bm_log, LogType
 import re
 import pandas as pd
 from bm_visualize import plot_chart, PlotConfig
+
 # TODO: pass along information about apps names to the monitors?
 
 
 class BpfTrace(Monitor):
     RESOURCES_PATH = "bm-runner/monitors/resources"
 
-
     def __init__(self, output_dir: str, args: list[str] = []):
         super().__init__(dir=output_dir, args=args)
         self.name = "bpftrace"
         self.traces = {}
-        self.count_pattern = re.compile(r"@(?P<name>\w+)\[(?P<pid>\d+),\s*(?P<comm>\w+)\]:\s*(?P<count>\d+)")
+        self.count_pattern = re.compile(
+            r"@(?P<name>\w+)\[(?P<pid>\d+),\s*(?P<comm>\w+)\]:\s*(?P<count>\d+)"
+        )
         for bt in args:
             cmds = ["sudo", "bpftrace"]
             out_name = f"{bt}.txt"
@@ -57,25 +58,31 @@ class BpfTrace(Monitor):
                 )
 
     def collect_results(self) -> str:
+        output = ""
         for name, trace in self.traces.items():
-            self.__parse_trace(name, trace)
-        # plot_perf_hist_for_comm(self.trace.output_file_name, "rocksdb_min_roc", os.path.join(self.dir, "bpftrace.png"))
-        return ""
-
+            output += self.__parse_trace(name, trace)
+        return output
 
     def __parse_trace(self, name: str, trace: BackgroundProcess) -> str:
+        output = ""
         count_trace = name.endswith("_count.bt")
         if count_trace:
             with open(trace.output_file_name, "r") as f:
                 contents = f.read()
-                df  = pd.DataFrame(
-                    m.groupdict()  for m in self.count_pattern.finditer(contents)
-                )
+                df = pd.DataFrame(m.groupdict() for m in self.count_pattern.finditer(contents))
                 if not df.empty:
-                    cfg = PlotConfig(x="pid", y="count", hue="pid", shape="barplot", title=name)
+                    counter_subject = df["name"].unique()
+                    if len(counter_subject) > 1:
+                        bm_log("Multiple counters detected, this case is not handled", LogType.FATAL)
+                        os.sys.exit(1)
+                    assert len(counter_subject) == 1, "No counter detected!"
+                    counter_subject = counter_subject[0]
+                    cfg = PlotConfig(x="pid", x_lbl="PID", y_lbl="Count", y="count", hue="pid", shape="barplot", title=counter_subject)
                     df["count"] = df["count"].astype(int)
-                    print(df)
                     plot_chart(plot=cfg, df=df, out_fig_name=trace.output_file_name)
-                    bm_log(f"{trace.output_file_name}", LogType.FATAL)
-
-
+                    count_col = df["count"]
+                    output+= f"{counter_subject}_sum={count_col.sum()};"
+                    output+= f"{counter_subject}_avg={count_col.mean()};"
+                    output+= f"{counter_subject}_min={count_col.min()};"
+                    output+= f"{counter_subject}_max={count_col.max()};"
+        return output
