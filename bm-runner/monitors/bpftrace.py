91473eeb066b8a67129870f462997ed1f1e77be0
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-06-29 12:33:26 +0300

    refactor
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/monitors/bpftrace.py
+++ bm-runner/monitors/bpftrace.py
@@ -29,27 +29,38 @@ class BpfTrace(Monitor):
             r"@(?P<name>\w+)\[(?P<pid>\d+),\s*(?P<comm>\w+)\]:\s*(?P<count>\d+)"
         )
 
+        self.hist_header = re.compile(
+            r"@(?P<name>\w+)\[(?P<pid>\d+),\s*(?P<comm>[^\]]+)\]:"
+        )
+        self.hist_bucket = re.compile(
+            r"\[(?P<bucket>[^\]]+)\]\s+(?P<count>\d+)"
+        )
+
         for bt in args:
             cmds = ["sudo", "bpftrace"]
             out_name = f"{bt}.txt"
             err_name = f"{bt}.err"
             bt_file = str(resolve_path(os.path.join(self.RESOURCES_PATH, f"{self.name}/{bt}")))
 
-            with open(bt_file, "r") as f:
-                # only first 16 chars (including null terminator) are in comm.
-                contents = f.read().replace("__FILTER__", self.__get_filter())
-                cmds.append("-e")
-                cmds.append(contents)
-                trace = BackgroundProcess(
-                    name=self.name,
-                    ofile_name=out_name,
-                    efile_name=err_name,
-                    cmds=cmds,
-                    out_dir=output_dir,
-                    requires=["bpftrace"],
-                    pin=self.get_cpus(),
-                )
-                self.traces[bt] = trace
+            try:
+                with open(bt_file, "r") as f:
+                    # only first 16 chars (including null terminator) are in comm.
+                    contents = f.read().replace("__FILTER__", self.__get_filter())
+                    cmds.append("-e")
+                    cmds.append(contents)
+                    trace = BackgroundProcess(
+                        name=self.name,
+                        ofile_name=out_name,
+                        efile_name=err_name,
+                        cmds=cmds,
+                        out_dir=output_dir,
+                        requires=["bpftrace"],
+                        pin=self.get_cpus(),
+                    )
+                    self.traces[bt] = trace
+            except Exception as e:
+                bm_log(f"{self.name} cannot read/parse {bt_file}. Exception {e}", LogType.FATAL)
+                sys.exit(1)
 
     def __get_filter(self) -> str:
         if bm_config.g_config is None:
@@ -86,35 +97,47 @@ class BpfTrace(Monitor):
         return output
 
     def __parse_trace(self, name: str, trace: BackgroundProcess) -> str:
-        output = ""
         count_trace = name.endswith("_count.bt")
-        if count_trace:
-            with open(trace.output_file_name, "r") as f:
-                contents = f.read()
-                df = pd.DataFrame(m.groupdict() for m in self.count_pattern.finditer(contents))
-                if not df.empty:
-                    counter_subject = df["name"].unique()
-                    if len(counter_subject) > 1:
-                        bm_log(
-                            "Multiple counters detected, this case is not handled", LogType.FATAL
-                        )
-                        sys.exit(1)
-                    assert len(counter_subject) == 1, "No counter detected!"
-                    counter_subject = counter_subject[0]
-                    cfg = PlotConfig(
-                        x="pid",
-                        x_lbl="PID",
-                        y_lbl="Count",
-                        y="count",
-                        hue="pid",
-                        shape="barplot",
-                        title=counter_subject,
-                    )
-                    df["count"] = df["count"].astype(int)
-                    plot_chart(plot=cfg, df=df, out_fig_name=trace.output_file_name)
-                    count_col = df["count"]
-                    output += f"{counter_subject}_sum={count_col.sum()};"
-                    output += f"{counter_subject}_avg={count_col.mean()};"
-                    output += f"{counter_subject}_min={count_col.min()};"
-                    output += f"{counter_subject}_max={count_col.max()};"
+        hist_trace = name.endswith("_hist.bt")
+        with open(trace.output_file_name, "r") as f:
+            content = f.read()
+            if count_trace:
+               return self.__parse_count(content, trace.output_file_name)
+            elif hist_trace:
+                return self.__parse_hist(content)
+        return ""
+
+
+    def __parse_count(self, content:str, fname) -> str:
+        output = ""
+        df = pd.DataFrame(m.groupdict() for m in self.count_pattern.finditer(content))
+        if not df.empty:
+            counter_subject = df["name"].unique()
+            if len(counter_subject) > 1:
+                bm_log(
+                    "Multiple counters detected, this case is not handled", LogType.FATAL
+                )
+                sys.exit(1)
+            assert len(counter_subject) == 1, "No counter detected!"
+            counter_subject = counter_subject[0]
+            cfg = PlotConfig(
+                x="pid",
+                x_lbl="PID",
+                y_lbl="Count",
+                y="count",
+                hue="pid",
+                shape="barplot",
+                title=counter_subject,
+            )
+            df["count"] = df["count"].astype(int)
+            plot_chart(plot=cfg, df=df, out_fig_name=fname)
+            count_col = df["count"]
+            output += f"{counter_subject}_sum={count_col.sum()};"
+            output += f"{counter_subject}_avg={count_col.mean()};"
+            output += f"{counter_subject}_min={count_col.min()};"
+            output += f"{counter_subject}_max={count_col.max()};"
         return output
+
+    def __parse_hist(self, content:str) -> str:
+        bm_log("Histogram parsing", LogType.FATAL)
+        return ""
